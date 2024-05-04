7408f2a5594c90c06e6639523106afe931726b9c
thanhbinh911 <167856446+thanhbinh911@users.noreply.github.com>
2024-05-04 10:55:48 +0700

    using greedy approach

--- minimum jump.cpp
+++ minimum jump.cpp
@@ -1,30 +1,38 @@
 #include<stdio.h>
-#include<climits>
-int n;
-int min (int a, int b) {
-	return a<b?a:b;
+
+int max(int a, int b) {
+	return a>b?a:b;
 }
 
-int minimum_jumps(int arr[]) {
-	int dp[n+1];
-	dp[n]=0;	
-	for(int i=n-1; i>=0; i--) {
-		dp[i]=INT_MAX;
-		for(int j=i+1; j<=i+arr[i]; j++) {	
-			if(j<=n && dp[j]!=INT_MAX)		
-				dp[i]=min(dp[i], dp[j]+1);
+int minimum_jumps(int arr[], int n) {
+	if(n<=1)
+		return 0;
+	if(arr[0]==0)
+		return -1;
+	if(arr[0]>=n-1)
+		return 1;
+	int max_reach=arr[0];
+	int jumps=1;
+	int step=arr[0];
+	for(int i=1; i<n; i++) {
+		if(i==n-1)
+			return jumps;
+		if(arr[i]>=n-i)
+			return jumps+1;
+		max_reach=max(max_reach, i+arr[i]);
+		step--;
+		if(step==0) {
+			jumps++;
+		if(i>=max_reach)
+			return -1;
+		step=max_reach-i;		
 		}
 	}
-	return dp[0];
+	return -1;
 }
 
 int main () {
-	int arr[]={4,0,1,2,0,2,1}; 
-	int length=sizeof(arr)/sizeof(arr[0]);
-	n=length-1;
-	int minimal=minimum_jumps(arr);
-	if(minimal==INT_MAX)
-		printf("0");
-	else
-	printf("%d", minimal);
+	int arr[]={3,4,1,3,1,0,1,1};
+	int size=sizeof(arr)/sizeof(arr[0]);
+	printf("%d", minimum_jumps(arr,size));
 }
